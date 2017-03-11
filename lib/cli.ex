acf9c5eabb87af7c6b6beb8f5f697dde944f0ecb
Zuzana Dostalova <dostalovaz@gmail.com>
2017-03-11 14:49:37 +0100

    Do help and do requests

--- lib/cli.ex
+++ lib/cli.ex
@@ -3,9 +3,15 @@ defmodule LoadTester.CLI do
   require Logger
 
   def main(args) do
+    Application.get_env(:load_tester, :master_node)
+      |> Node.start
+
+    Application.get_env(:load_tester, :slave_nodes)
+      |> Enum.each(fn x -> Node.connect(x) end)
+
     args
       |> parse_args
-      |> process_options
+      |> process_options([node|Node.list])
   end
 
   defp parse_args(args) do
@@ -14,8 +20,37 @@ defmodule LoadTester.CLI do
 
   defp process_options(options, nodes) do
     case options do
-      {[requests: n], [url], []} -> #perform action
+      {[requests: n], [url], []} -> do_requests(n, url, nodes)
       _ -> do_help
     end
+  end
+
+  defp do_help do
+    IO.puts """
+      Usage:
+      load_tester -n [requests] [url]
+
+      Options:
+      -n, [--requests]                 # Number of requests
+
+      Example:
+      ./load_tester 5 "http://journeyapp.net"
+    """
+    System.halt(0)
+  end
+
+  defp do_requests(n, url, nodes) do
+    Logger.info "Pummeling #{url} with #{n} requests."
+    total_nodes = Enum.count(nodes)
+    req_per_node = div(n, total_nodes)
+
+    nodes
+      |> Enum.flat_map(fn node ->
+        1..req_per_node |> Enum.map(fn _ -> 
+          Task.Supervisor.async({LoadTester.TaskSupervisor, node}, LoadTester.Worker, :start, [url]) end
+        ) end
+      )
+      |> Enum.map(fn x -> Task.await(x, :infinity) end)
+      |> parse_results
   end
 end
