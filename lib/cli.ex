662a1a26138ae78c55d649924d0955d70815742c
Zuzana Dostalova <dostalovaz@gmail.com>
2017-03-12 17:48:50 +0100

    Finished but not working

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,5 +1,5 @@
-use Mix.config
 defmodule LoadTester.CLI do
+  alias LoadTester.TasksSupervisor
   require Logger
 
   def main(args) do
@@ -11,17 +11,17 @@ defmodule LoadTester.CLI do
 
     args
       |> parse_args
-      |> process_options([node|Node.list])
+      |> process_options([node()|Node.list])
   end
 
   defp parse_args(args) do
-    OptionParser.parse(args, aliases: [n: :requests], strict: [:requests: :integer])
+    OptionParser.parse(args, aliases: [n: :requests], strict: [requests: :integer])
   end
 
   defp process_options(options, nodes) do
     case options do
       {[requests: n], [url], []} -> do_requests(n, url, nodes)
-      _ -> do_help
+      _ -> do_help()
     end
   end
 
@@ -34,23 +34,63 @@ defmodule LoadTester.CLI do
       -n, [--requests]                 # Number of requests
 
       Example:
-      ./load_tester 5 "http://journeyapp.net"
+      ./load_tester -n 5 "http://journeyapp.net"
     """
     System.halt(0)
   end
+
+  require IEx
 
   defp do_requests(n, url, nodes) do
     Logger.info "Pummeling #{url} with #{n} requests."
     total_nodes = Enum.count(nodes)
     req_per_node = div(n, total_nodes)
 
-    nodes
+    Node.list
       |> Enum.flat_map(fn node ->
         1..req_per_node |> Enum.map(fn _ -> 
-          Task.Supervisor.async({LoadTester.TaskSupervisor, node}, LoadTester.Worker, :start, [url]) end
+          Task.Supervisor.async({LoadTester.TasksSupervisor, node}, LoadTester.Worker, :start, [url]) end
         ) end
       )
       |> Enum.map(fn x -> Task.await(x, :infinity) end)
       |> parse_results
+  end
+
+  defp parse_results(results) do
+    {successes, _failures} =
+      results
+        |> Enum.partition(fn x ->
+          case x do
+            {:ok, _ } -> true
+            _         -> false
+          end
+        end )
+
+    total_workers = Enum.count(results)
+    total_success = Enum.count(successes)
+    total_failure = total_workers - total_success
+
+    data = successes |> Enum.map(fn {:ok, time } -> time end )
+    average_time  = average(data)
+    longest_time  = Enum.max(data)
+    shortest_time = Enum.min(data)
+
+    IO.puts """
+      Total workers:       #{total_workers}
+      Successful requests: #{total_success}
+      Failed requests:     #{total_failure}
+      Average (msec):      #{average_time}
+      Longest (msec):      #{longest_time}
+      Shortest (msec):     #{shortest_time}
+    """
+  end
+
+  defp average(list) do
+    sum = Enum.sum(list)
+    if sum > 0 do
+      sum / Enum.count(list)
+    else
+      0
+    end
   end
 end
--- lib/load_tester.ex
+++ lib/load_tester.ex
@@ -1,61 +1,7 @@
 defmodule LoadTester do
-  @moduledoc """
-  Documentation for LoadTester.
-  """
+  use Application
 
-  @doc """
-  Measure the response time of a n GET requests for given url.
-
-  ## Examples
-
-      iex> LoadTester.run(5, 'http://google.com')
-      [{:ok, 212.233}, {:ok, 130.746}, {:ok, 126.054}, {:ok, 187.325}, {:ok, 159.102}]
-
-  """
-  def run(n_workers, url) when n_workers > 0 do
-    worker_func = fn -> LoadTester.Worker.start(url) end
-
-    1..n_workers
-      |> Enum.map(fn _ -> Task.async(worker_func) end)
-      |> Enum.map(fn x -> Task.await(x) end)
-      |> parse_results
-  end
-
-  defp parse_results(results) do
-    {successes, _failures} =
-      results
-        |> Enum.partition(fn x ->
-          case x do
-            {:ok, _ } -> true
-            _         -> false
-          end
-        end )
-
-    total_workers = Enum.count(results)
-    total_success = Enum.count(successes)
-    total_failure = total_workers - total_success
-
-    data = successes |> Enum.map(fn {:ok, time } -> time end )
-    average_time  = average(data)
-    longest_time  = Enum.max(data)
-    shortest_time = Enum.min(data)
-
-    IO.puts """
-      Total workers:       #{total_workers}
-      Successful requests: #{total_success}
-      Failed requests:     #{total_failure}
-      Average (msec):      #{average_time}
-      Longest (msec):      #{longest_time}
-      Shortest (msec):     #{shortest_time}
-    """
-  end
-
-  defp average(list) do
-    sum = Enum.sum(list)
-    if sum > 0 do
-      sum / Enum.count(list)
-    else
-      0
-    end
+  def start(_type, _args) do
+    LoadTester.Supervisor.start_link(:ok)
   end
 end
